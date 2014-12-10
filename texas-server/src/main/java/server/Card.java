aac423fcfa2b78977ba29cad2b590e9c909a3623
Michaelbest1 <Michaelbest002@126.com>
2014-12-10 10:28:48 +0800

    Modify Card

--- texas-server/src/main/java/server/Card.java
+++ texas-server/src/main/java/server/Card.java
@@ -1,10 +1,22 @@
 package server;
 
-public class Card {
+import util.Util;
+
+public class Card implements Comparable<Card>{
 	private String suit;
 	private int point;
 	
 	public Card(String suit, int point) {
+		if (point < 2 || point > 14) {
+			System.out.println("Error when creating Card. Invalid point!");
+			return;
+		}
+		
+		if (!suit.equals("S") && !suit.equals("H") && !suit.equals("C") && !suit.equals("D")) {
+			System.out.println("Error when creating Card. Invalid suit!");
+			return;
+		}
+		
 		this.suit = new String(suit);
 		this.point = point;
 	}
@@ -21,10 +33,19 @@ public class Card {
 	public void setPoint(int point) {
 		this.point = point;
 	}
-
+	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return this.point + this.suit;
+	}
+
+	public int compareTo(Card o) {
+		// TODO Auto-generated method stub
+		if (getPoint() == o.getPoint()){
+			return Util.suitToInt(getSuit()) - Util.suitToInt(o.getSuit());
+		}
+		
+		return getPoint() - o.getPoint();
 	}	
 }
